bc00eb6cd58b0948faf0f31cb8f632bca5a7842d
John <kai.wang.john@gmail.com>
2014-04-24 13:34:34 +0000

    add to linode

---
+++ config/config.php
@@ -0,0 +1,46 @@
+<?php
+$config["esoTalk.installed"] = true;
+$config["esoTalk.version"] = '1.0.0g3';
+$config["esoTalk.database.host"] = 'localhost';
+$config["esoTalk.database.user"] = 'root';
+$config["esoTalk.database.password"] = 'Super.user';
+$config["esoTalk.database.dbName"] = 'gezhongma';
+$config["esoTalk.database.prefix"] = 'et_';
+$config["esoTalk.forumTitle"] = '各种码';
+$config["esoTalk.baseURL"] = 'http://106.186.22.35/gezhongma/';
+$config["esoTalk.emailFrom"] = 'do_not_reply@106.186.22.35';
+$config["esoTalk.cookie.name"] = '';
+$config["esoTalk.urls.friendly"] = true;
+$config["esoTalk.urls.rewrite"] = false;
+$config["BBCode.version"] = '1.0.0g3';
+$config["ReportBug.version"] = '1.0.0g3';
+$config["esoTalk.admin.lastUpdateCheckTime"] = 1398334345;
+$config["esoTalk.admin.welcomeShown"] = true;
+$config["esoTalk.enabledPlugins"] = array (
+  0 => 'BBCode',
+  2 => 'Emoticons',
+  3 => 'ChannelNotifications',
+  5 => 'Likes',
+  6 => 'MemberNotifications',
+  7 => 'WordFilter',
+  8 => 'Gravatar',
+  9 => 'AboutMe',
+);
+$config["Emoticons.version"] = '1.0.0g3';
+$config["ChannelNotifications.version"] = '1.0.0g3';
+$config["Gravatar.version"] = '1.0.0g3';
+$config["Likes.version"] = '1.0.0g3';
+$config["MemberNotifications.version"] = '1.0.0g3';
+$config["WordFilter.version"] = '1.0.0g3';
+$config["esoTalk.language"] = 'Chinese';
+$config["esoTalk.forumLogo"] = false;
+$config["esoTalk.defaultRoute"] = 'conversations';
+$config["esoTalk.registration.open"] = '1';
+$config["esoTalk.registration.requireEmailConfirmation"] = '';
+$config["esoTalk.members.visibleToGuests"] = '0';
+$config["esoTalk.conversation.editPostTimeLimit"] = -1;
+$config["plugin.Gravatar.default"] = 'retro';
+$config["AboutMe.version"] = '1.0.0g3';
+
+// Last updated by: john (61.149.231.96) @ Thu, 24 Apr 2014 10:12:38 +0000
+?>
